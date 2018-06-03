80c0a8e3ef786af4c913aeebb090dc96cbc97fe1
Ashley <AshleyReneeLowry@gmail.com>
2018-06-03 08:55:53 -0500

    Rewrite test to use enzyme

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 
 import App from '../App';
+import CommentBox from '../CommentBox';
 
 describe('App Component Tests', () => {
-  it('should render component', () => {
-    const div = document.createElement('div');
-
-    // Can we render an instance of App Component?
-    ReactDOM.render(<App />, div);
-
-    // "Clean up" Remove component from the JSDOM
-    ReactDOM.unmountComponentAtNode(div);
-  });
-
   it('should show the comment box', () => {
-    const div = document.createElement('div');
-
-    ReactDOM.render(<App />, div);
-
-    // Look inside div and check to see if CommentBox is in there?
-    expect(div.innerHTML).toContain('Comment Box');
-
-    ReactDOM.unmountComponentAtNode(div);
+    // Using Enzyme
+    const wrapped = shallow(<App />);
+    // Find returns an array
+    expect(wrapped.find(CommentBox).length).toEqual(1);
   });
 });
