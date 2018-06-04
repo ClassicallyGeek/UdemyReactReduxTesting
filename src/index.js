38ee588e17553b58c6bf5061aaaf1968d14f23e5
Ashley <AshleyReneeLowry@gmail.com>
2018-06-04 11:01:43 -0500

    Add redux to app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+/* Provider is a react component that communicates directly with every connect
+function we create in our app. The connect function can reach to the provider
+and get access to state, or dispatch actions via action creator */
+import { createStore } from 'redux';
 
+import reducers from 'reducers'; // when we import a director, we get the index.js file in that dir.
 import App from 'components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(
+  <Provider store={ createStore(reducers, {}) }>
+    <App />
+  </Provider>, document.querySelector('#root')
+);
